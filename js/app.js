01cab10cb6afd5987bc8546bd2093d76228a64c3
Andraser <rubens.bovalo@gmail.com>
2017-07-26 15:59:03 +0400

    nicesuuu

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
 //global variable
-let carte = ['img/1.jpg','img/1.jpg','img/2.jpg','img/2.jpg','img/3.jpg','img/3.jpg', 'img/4.jpg', 'img/4.jpg' ,'img/5.jpg' ,'img/5.jpg' ,'img/6.jpg' ,'img/6.jpg' , 'img/7.jpg', 'img/7.jpg'];
+let carte = ['img/1.jpg','img/1.jpg','img/2.jpg','img/2.jpg','img/3.jpg',
+'img/3.jpg', 'img/4.jpg', 'img/4.jpg' ,'img/5.jpg' ,'img/5.jpg'
+,'img/6.jpg' ,'img/6.jpg' , 'img/7.jpg', 'img/7.jpg'];
 let arr = carte;
+let card = [];
 shuffle(arr);
 
 // mélange des cartes
@@ -10,7 +13,7 @@ function shuffle(arr) {
 }
 console.log(shuffle([1, 1, 2, 2, 3, 3, 4, 4, 5 , 5 , 6 , 6 , 7 , 7]))
 
-// recupere les images
+// recupere les images et affiche les images
 
 let recupImg = document.querySelectorAll('img');
 for (let i = 0; i < carte.length; i++){
@@ -18,4 +21,9 @@ for (let i = 0; i < carte.length; i++){
 		e.currentTarget.setAttribute("src", carte[i]);})
 }
 
-//comparaison
+//comparaison quand les cartes sont flip
+if (carte.length === 2){
+  (carte[0] === carte[1])
+} else {
+                                                                              
+}
